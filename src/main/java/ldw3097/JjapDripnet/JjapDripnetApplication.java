48ceec5ab38be628bdbd174589dd4ab060ffb93f
ldw <ldw3097@gmail.com>
2023-12-29 17:05:29 +0900

    Init
    initialized by springboot

---
+++ src/main/java/ldw3097/JjapDripnet/JjapDripnetApplication.java
@@ -0,0 +1,13 @@
+package ldw3097.JjapDripnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjapDripnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjapDripnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ldw3097/JjapDripnet/JjapDripnetApplicationTests.java
@@ -0,0 +1,13 @@
+package ldw3097.JjapDripnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjapDripnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
